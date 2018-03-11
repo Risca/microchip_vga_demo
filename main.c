7e498b287342ce2985a66ba2d54540f36d12d9fa
Patrik Dahlström <risca@powerlamerz.org>
2018-03-11 03:59:48 +0100

    restructuring

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <xc.h>
 __PROG_CONFIG(1, 0x184);
 
+enum Scene {
+	SCENE_BOX,
+	SCENE_BLUE,
+};
+enum Scene g_CurrentScene = SCENE_BOX;
+
 void __interrupt() isr(void)
 {
 	// statements in this order due to timing
@@ -21,6 +27,77 @@ void __interrupt() isr(void)
 		TMR1L = 0xFF - 0x2A;
 		T1CON = 0x31;
 	}
+}
+
+void draw_box(void)
+{
+	asm("movlw	7");
+	asm("movwf	5");
+	__delay_us(30);
+	asm("nop");
+	asm("clrf	5");
+
+#define START_OFFSET 320
+	__delay_us(START_OFFSET);
+
+	__delay_us(320);
+
+	for (char i = 0; i < 49; ++i) {
+		__delay_us(8);
+#asm
+	bcf	status, 5	;RP0=0, select bank0
+	movlw	7
+	movwf	5
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	movlw	1
+	movwf   5
+	movlw	2
+	movwf	5
+	movlw	4
+	movwf	5
+	clrf	5
+	movlw	7
+	movwf	5
+	nop
+	nop
+	nop
+	nop
+	nop
+	clrf	5
+#endasm	
+		__delay_us(7);
+	}
+
+	__delay_ms(12);
+	__delay_us(1216 - START_OFFSET);
+	__delay_us(32);
+	__delay_us(32);
+
+	asm("movlw	7");
+	asm("movwf	5");
+	__delay_us(14);
+	asm("movlw	1");
+	asm("movwf	5");
+	asm("movlw	7");
+	asm("movwf	5");
+	__delay_us(14);
+	asm("nop");
+	asm("clrf	5");
+}
+
+void blue_screen(void)
+{
+	asm("movlw	4");
+	asm("movwf	5");
+	__delay_ms(15);
+	__delay_us(350);
+	asm("clrf	5");
 }
 
 void main(void)
@@ -52,27 +129,32 @@ void main(void)
 
 	INTCON = 0xC0;
 
-#define START_OFFSET 119
+	__delay_ms(13);
+	__delay_us(365);
+	NOP();
 	while (1) {
-		__delay_us(START_OFFSET);
+		// delay 35 lines - VSYNC + back porch - switch-case
+		__delay_ms(1);
+		__delay_us(123);
+		asm("nop");
 
-		for (int i = 1; i < 51; ++i) {
-			__delay_us(5);
-			PORTA = i >> 2;
-			__delay_us(10);
-			PORTA = 0;
-			__delay_us(9);
-			NOP();
-
-			__delay_us(5);
-			PORTA = i >> 2;
-			__delay_us(10);
-			PORTA = 0;
-			NOP();
+		switch (g_CurrentScene) {
+		case SCENE_BOX:
+			draw_box();
+			break;
+		case SCENE_BLUE:
+			blue_screen();
+			break;
 		}
-
-		__delay_ms(12);
-		__delay_us(1389 - START_OFFSET);
+		asm("nop");
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(32);
+		__delay_us(7); // front porch
 	}
 }
 
